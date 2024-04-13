6f38a0fc843418ace0a847e3de70abdcfb371052
theRealViner <ltl.viner@gmail.com>
2024-04-13 15:54:52 +0100

    added get_property_details method that gets all the details of a property given the property id

--- testingAPI.py
+++ testingAPI.py
@@ -22,6 +22,12 @@ function to get the location,key dict for user inputted area name
 arguments:
     location: the area name you want to search for (user enters)
     num_results: the number of results you want to return
+
+returns:
+    a list of dictionaries containing the location and key
+
+    example:
+    [{'location': 'New Haw', 'key':new-haw}]
 '''
 def get_location_keys(location, num_results=1):
     conn.request("GET", f"/locations?location={location}", headers=headers)
@@ -46,6 +52,13 @@ arguments:
     sort: the sorting method for the results (options: "recent", 
                                                 "highest", "lowest")
     page: the page number of the results
+    fetch_all: whether to fetch all the results or just the given page
+
+returns:
+    a list of property ids that match the given criteria
+
+    example:
+    [123456, 123457, 123458, 123459]
 '''
 def get_property_ids(location_key, min_price=0, max_price=10000,
                      min_beds=1, max_beds=4, sort="recent", page="1",fetch_all=False):
@@ -75,6 +88,28 @@ def get_property_ids(location_key, min_price=0, max_price=10000,
 
     return out
 
+'''
+function to get the property details for a given property id
+arguments:
+    property_id: the id of the property you want to get the details for
+
+returns:
+    a dictionary containing the property details
+
+    example:
+    ###see apis in discord###
+'''
+def get_property_details(property_id):
+    try:
+        conn.request("GET", f"/properties/{property_id}", headers=headers)
+        res = conn.getresponse()
+        data = res.read()
+        unpacked = json.loads(data.decode("utf-8"))['data']
+    except:
+        unpacked = None
+    return unpacked
+
 #get_location_keys("new-haw",num_results=20)
-get_property_ids("walton-on-thames",min_price=100,max_price=10000,min_beds=2,max_beds=4,sort="recent",page="1",fetch_all=True)
+#get_property_ids("walton-on-thames",min_price=100,max_price=10000,min_beds=2,max_beds=4,sort="recent",page="1",fetch_all=True)
+get_property_details(66703015)
 print("stop")
