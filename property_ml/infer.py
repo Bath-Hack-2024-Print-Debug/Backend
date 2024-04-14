162d96db8d4147c1c833e41d304340337758e60e
theRealViner <ltl.viner@gmail.com>
2024-04-14 03:58:56 +0100

    fixed pandas issues and made infer.py into a function

--- property_ml/infer.py
+++ property_ml/infer.py
@@ -17,93 +17,96 @@ def series_to_string_list(row_series):
     
     return quoted_list
 
-df = pd.read_csv('bath_ml.csv')
-df = df[[
-      "id",
-      "propertyType",
-      "name",
-      "publishedOn",
-      "address",
-      "latitude",
-      "longitude",
-      "priceActual",
-      "priceMax",
-      "priceMin",
-      "livingRooms",
-      "beds",
-      "bedsMax",
-      "bedsMin",
-      "isRetirementHome",
-      "isSharedOwnership",
-      "studentFriendly"]]
-df.set_index('id', inplace=True)
+def inferRent(property_id):
+  df = pd.read_csv('./property_ml/bath_ml.csv')
+  df = df[[
+        "id",
+        "propertyType",
+        "name",
+        "publishedOn",
+        "address",
+        "latitude",
+        "longitude",
+        "priceActual",
+        "priceMax",
+        "priceMin",
+        "livingRooms",
+        "beds",
+        "bedsMax",
+        "bedsMin",
+        "isRetirementHome",
+        "isSharedOwnership",
+        "studentFriendly"]]
+  df.set_index('id', inplace=True)
 
-input_row = df.iloc[0]
-result = series_to_string_list(input_row)
-print(result)
+  input_row = df.loc[property_id]
+  result = series_to_string_list(input_row)
+  print(result)
 
-print("pause")
+  print("pause")
 
 
-def allowSelfSignedHttps(allowed):
-    # bypass the server certificate verification on client side
-    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
-        ssl._create_default_https_context = ssl._create_unverified_context
+  def allowSelfSignedHttps(allowed):
+      # bypass the server certificate verification on client side
+      if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
+          ssl._create_default_https_context = ssl._create_unverified_context
 
-allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+  allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
 
-# Request data goes here
-# The example below assumes JSON formatting which may be updated
-# depending on the format your endpoint expects.
-# More information can be found here:
-# https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
-data =  {
-  "input_data": {
-    "columns": [
-      "propertyType",
-      "name",
-      "publishedOn",
-      "address",
-      "latitude",
-      "longitude",
-      "priceActual",
-      "priceMax",
-      "priceMin",
-      "livingRooms",
-      "beds",
-      "bedsMax",
-      "bedsMin",
-      "isRetirementHome",
-      "isSharedOwnership",
-      "studentFriendly"
-    ],
-    "index": [f"{df.index[0]}"],
-    "data": [result]
+  # Request data goes here
+  # The example below assumes JSON formatting which may be updated
+  # depending on the format your endpoint expects.
+  # More information can be found here:
+  # https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
+  data =  {
+    "input_data": {
+      "columns": [
+        "propertyType",
+        "name",
+        "publishedOn",
+        "address",
+        "latitude",
+        "longitude",
+        "priceActual",
+        "priceMax",
+        "priceMin",
+        "livingRooms",
+        "beds",
+        "bedsMax",
+        "bedsMin",
+        "isRetirementHome",
+        "isSharedOwnership",
+        "studentFriendly"
+      ],
+      "index": [f"{df.index[0]}"],
+      "data": [result]
+    }
   }
-}
 
-body = str.encode(json.dumps(data))
+  body = str.encode(json.dumps(data))
 
-url = 'https://full-hack-bath-infer.uksouth.inference.ml.azure.com/score'
-# Replace this with the primary/secondary key or AMLToken for the endpoint
-api_key = '4bfCay1HlUXmbSF0s6oHylrxOcQZaD21'
-if not api_key:
-    raise Exception("A key should be provided to invoke the endpoint")
+  url = 'https://full-hack-bath-infer.uksouth.inference.ml.azure.com/score'
+  # Replace this with the primary/secondary key or AMLToken for the endpoint
+  api_key = '4bfCay1HlUXmbSF0s6oHylrxOcQZaD21'
+  if not api_key:
+      raise Exception("A key should be provided to invoke the endpoint")
 
-# The azureml-model-deployment header will force the request to go to a specific deployment.
-# Remove this header to have the request observe the endpoint traffic rules
-headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'infer-point' }
+  # The azureml-model-deployment header will force the request to go to a specific deployment.
+  # Remove this header to have the request observe the endpoint traffic rules
+  headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'infer-point' }
 
-req = urllib.request.Request(url, body, headers)
+  req = urllib.request.Request(url, body, headers)
 
-try:
-    response = urllib.request.urlopen(req)
+  try:
+      response = urllib.request.urlopen(req)
 
-    result = response.read()
-    print(result)
-except urllib.error.HTTPError as error:
-    print("The request failed with status code: " + str(error.code))
+      result = response.read()
+      print(result)
+  except urllib.error.HTTPError as error:
+      print("The request failed with status code: " + str(error.code))
 
-    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-    print(error.info())
-    print(error.read().decode("utf8", 'ignore'))
+      # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+      print(error.info())
+      print(error.read().decode("utf8", 'ignore'))
+
+print(inferRent(66990268))
